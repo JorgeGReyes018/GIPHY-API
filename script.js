12517290be6b69badd28596f7bdab157519f43f7
JorgeGReyes <JorgeGReyes018@yahoo.com>
2022-10-27 16:44:13 -0400

    update 1 of JS API file

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 let key = "4Q4m7pj8IPe8DFPLfknrbhZQuDl8tMLH"
 let output = document.querySelector(".out")
+document.addEventListener("DOMContentLoaded",search)
+function search() {
+    document.getElementById("btnS").addEventListener("click", getData => {
+        getData.preventDefault();
+        output.innerHTML = "";
+        let val = document.getElementById("search").value;
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&limit=60&q=${val}`;
+        console.log(url);
+    } );
+}
