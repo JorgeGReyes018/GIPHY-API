9e09b25a9f54575618ef4d31581f96bac47de5e6
JorgeGReyes <JorgeGReyes018@yahoo.com>
2022-10-27 21:58:39 -0400

    css and JS
    finished adding the JavaScript for API to function. also changed some sizing on CSS

--- script.js
+++ script.js
@@ -6,7 +6,21 @@ function search() {
         getData.preventDefault();
         output.innerHTML = "";
         let val = document.getElementById("search").value;
-        let url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&limit=60&q=${val}`;
+        let url = `https://api.giphy.com/v1/gifs/search?api_key=${key}&limit=49&q=${val}`;
         console.log(url);
+        fetch(url)
+        .then(response => response.json() )
+        .then(content => {
+            console.log(content.data);
+            content.data.forEach(function(info){
+                console.log(info.images.downsized_large.url);
+                let cont = document.createElement("img")
+                cont.setAttribute("src",info.images.downsized_large.url);
+                output.appendChild(cont)
+            })
+        })
+        .catch(error => {
+            console.log(error);
+        })
     } );
 }
